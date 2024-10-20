2611fed4551a4431f061e55628bf0b3a97b3767c
Nicolas <nicolas.moraga@chesstutorai.com>
2024-10-20 11:42:06 -0300

    base api

--- app/payment/main.py
+++ app/payment/main.py
@@ -1,10 +1,34 @@
 from fastapi import FastAPI
 
+import pymongo
+import os
+from dotenv import load_dotenv
+
 from ..routers.test import prefix, router
+
+load_dotenv()
+
+mongo_client = pymongo.MongoClient("mongodb://localhost:27017/",
+                                   username=os.getenv("MONGO_ADMIN_USER"),
+                                   password=os.getenv("MONGO_ADMIN_PASS"))
+
+db = mongo_client["payment"]
 
 app = FastAPI()
 app.include_router(router)
 
-@app.get(f"{prefix}/hello")
-def read_root():
-    return {"Hello": "World"}
+@app.get(f"{prefix}/{{student_id}}/payments")
+def get_payments(student_id: str):
+    #Listar pagos de un alumno
+    return {"Hello": student_id}
+
+@app.get(f"{prefix}/{{student_id}}/payments/{{payment_id}}")
+def get_payment(student_id: str, payment_id: str):
+    #Obtener un pago de un alumno
+    return {"Hello": student_id, "Payment": payment_id}
+
+
+@app.get(f"{prefix}/{{student_id}}/debt/{{payment_id}}/payments")
+def get_debt_payments(student_id: str, payment_id: str):
+    #Listar pagos pendientes de un alumno
+    return {"Hello": student_id, "Payment": payment_id}
